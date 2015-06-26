f2952aeadbb46b1dcb6579178c16235d984f2398
Todd Crone <todd.crone@perceptivesoftware.com>
2015-06-26 12:05:31 -0400

    add small color spheres dropping

--- SCNGame/GameSCNScene.swift
+++ SCNGame/GameSCNScene.swift
@@ -45,15 +45,15 @@ class GameSCNScene: SCNScene {
             chamferRadius: 0)
 
         let sphereGeometry = SCNSphere(radius: 1.0)
-        sphereGeometry.firstMaterial?.diffuse.contents = UIColor.orangeColor()
+        sphereGeometry.firstMaterial?.diffuse.contents = UIColor.purpleColor()
         
         let sphereNode = SCNNode(geometry: sphereGeometry)
-        sphereNode.position = SCNVector3Make(0.0, 10.0, -25.0)
+        sphereNode.position = SCNVector3Make(0.0, 10.0, -30.0)
         sphereNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: collisionBox, options: nil)
         sphereNode.physicsBody = SCNPhysicsBody.dynamicBody()
         sphereNode.physicsBody?.mass = 50
         //sphereNode.physicsBody?.angularVelocityFactor = SCNVector3Zero
-        sphereNode.physicsBody?.velocity = SCNVector3(x: 0.0, y: 0.0, z: 15.0)
+        sphereNode.physicsBody?.velocity = SCNVector3(x: 0.0, y: 0.0, z: 25.0)
         sphereNode.physicsBody?.rollingFriction = 0.5
         sphereNode.name = "bigSphere"
         scene.rootNode.addChildNode(sphereNode)
@@ -64,10 +64,10 @@ class GameSCNScene: SCNScene {
             chamferRadius: 0)
         
         let secondSphereGeometry = SCNSphere(radius: 0.5)
-        secondSphereGeometry.firstMaterial?.diffuse.contents = UIColor.yellowColor()
-
+        secondSphereGeometry.firstMaterial?.diffuse.contents = UIColor.blueColor()
+        let random = Float(arc4random_uniform(7)) - 4.0
         let secondSphereNode = SCNNode(geometry: secondSphereGeometry)
-        secondSphereNode.position = SCNVector3(x: 1.0, y: 0.0, z: 5.0)
+        secondSphereNode.position = SCNVector3(x: random, y: 10.0, z: 5.0)
         secondSphereNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: collisionBox, options: nil)
         secondSphereNode.physicsBody = SCNPhysicsBody.dynamicBody()
         secondSphereNode.physicsBody?.mass = 20
@@ -82,16 +82,42 @@ class GameSCNScene: SCNScene {
 //        secondSphereNode.runAction(repeatedSequence)
     }
     
+    var _counter = 0
+    
     func update() {
-        addBox()
+        _counter++
+        
+        if _counter % 5 == 0 {
+            addSmallSphere()
+        }
+        if _counter % 50 == 0 {
+            addBox()
+        }
+        if _counter % 200 == 0 {
+            addGeometryNode()
+            addThirdSphere()
+        }
+        
+        if _counter % 250 == 0 {
+            addSecondSphere()
+        }
+    }
+    
+    func randomColor() -> UIColor {
+        let random = Int(arc4random_uniform(3))
+        switch random {
+        case 1: return UIColor.orangeColor()
+        case 2: return UIColor.yellowColor()
+        default: return UIColor.greenColor()
+        }
     }
     
     func addBox() {
         let geo = SCNBox(width: 0.5, height: 0.5, length: 0.5, chamferRadius: 0.0)
-        geo.firstMaterial?.diffuse.contents = UIColor.redColor()
+        geo.firstMaterial?.diffuse.contents = randomColor()
         
         let boxNode = SCNNode(geometry: geo)
-        boxNode.position = SCNVector3Make(-1.0, 5.0, 6.0)
+        boxNode.position = SCNVector3Make(-1.0, 7.0, 6.0)
 //        boxNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: geo, options: nil)
         boxNode.physicsBody = SCNPhysicsBody.dynamicBody()
         boxNode.rotation = SCNVector4(x: 0.5, y: 0.15, z: 0.37, w: 0.5)
@@ -101,12 +127,29 @@ class GameSCNScene: SCNScene {
         boxNode.name = "box"
     }
     
+    func addSmallSphere() {
+        let geo = SCNSphere(radius: 0.25)
+        geo.firstMaterial?.diffuse.contents = randomColor()
+        
+        let boxNode = SCNNode(geometry: geo)
+        let randomX = Float(arc4random_uniform(7)) - 4.0
+        let randomZ = Float(arc4random_uniform(7))
+
+        boxNode.position = SCNVector3Make(randomX, 10.0, randomZ)
+        //        boxNode.physicsBody?.physicsShape = SCNPhysicsShape(geometry: geo, options: nil)
+        boxNode.physicsBody = SCNPhysicsBody.dynamicBody()
+        
+        scene.rootNode.addChildNode(boxNode)
+        
+        boxNode.name = "small"
+    }
+
     func addThirdSphere() {
         let collisionBox = SCNBox(width: 10.0, height: 10.0, length: 10.0,
             chamferRadius: 0)
         
         let secondSphereGeometry = SCNSphere(radius: 0.5)
-        secondSphereGeometry.firstMaterial?.diffuse.contents = UIColor.blueColor()
+        secondSphereGeometry.firstMaterial?.diffuse.contents = UIColor.redColor()
         
         let secondSphereNode = SCNNode(geometry: secondSphereGeometry)
         secondSphereNode.position = SCNVector3(x: 0.0, y: 15.0, z: 7.0)
